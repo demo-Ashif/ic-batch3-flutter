2eeb55a07dfffafc52eb3826521331093defa5e0
Ashif Mujtoba <nmujtoba@gmail.com>
2025-08-27 02:42:56 +0600

    code for google admob added 2

--- lib/ad_unit_ids.dart
+++ lib/ad_unit_ids.dart
@@ -1,12 +1,12 @@
 class AdUnitIds {
   // app id: ca-app-pub-7386117651253730~7940872997
-  static const bannerAndroidAdUnitId = 'ca-app-pub-7386117651253730/9613047314';
-  static const interstitialAndroidUnitId = 'ca-app-pub-7386117651253730/7617439365';
-  static const rewardAndroidUnitId = 'ca-app-pub-7386117651253730/4809127409';
+  static const bannerAndroidAdUnitId = 'ca-app-pub-7386117651253788/9613047314';
+  static const interstitialAndroidUnitId = 'ca-app-pub-7386117651003730/7617439365';
+  static const rewardAndroidUnitId = 'ca-app-pub-7386117651254430/4809127409';
 
   //
   // app id ios: ca-app-pub-7386117651253730~1239733274
-  static const bannerIosAdUnitId = 'ca-app-pub-7386117651253730/2935974236';
-  static const interstitialIosUnitId = 'ca-app-pub-7386117651253730/3879189116';
-  static const rewardIosUnitId = 'ca-app-pub-7386117651253730/7435290746';
+  static const bannerIosAdUnitId = 'ca-app-pub-7386117653353730/2935974236';
+  static const interstitialIosUnitId = 'ca-app-pub-7386447651253730/3879189116';
+  static const rewardIosUnitId = 'ca-app-pub-7386117666253730/7435290746';
 }
